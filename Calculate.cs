35221dfab8f2844267c76f9cd565a27bf58c0fd4
Skypper23 <165066461+Skypper23@users.noreply.github.com>
2025-07-14 16:44:48 +0100

    .
    .

---
+++ Calculate.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace wallhack_cs
+{
+    public static class Calculate
+    {
+        public static Vector2 WorldToScreen(ViewMatrix matrix, Vector3 pos, int width, int height)
+        {
+            float screenW = (matrix.m41 * pos.X) + (matrix.m42 * pos.Y) + (matrix.m43 * pos.Z) + matrix.m44;
+
+            if (screenW > 0.001f)
+            {
+                float screenX = (matrix.m11 * pos.X) + (matrix.m12 * pos.Y) + (matrix.m13 * pos.Z) + matrix.m14;
+                float screenY = (matrix.m21 * pos.X) + (matrix.m22 * pos.Y) + (matrix.m23 * pos.Z) + matrix.m24;
+
+                float camX = width * 0.5f; // Multiplicação mais rápida que divisão
+                float camY = height * 0.5f;
+
+                return new Vector2(
+                    camX + (camX * screenX / screenW),
+                    camY - (camY * screenY / screenW)
+                );
+            }
+
+            return new Vector2(-99, -99);
+        }
+    }
+}
---
+++ Entity.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace wallhack_cs
+{
+    public class Entity
+    {
+        public IntPtr pawnAddress;
+        public IntPtr controllerAddress;
+        public Vector3 origin;
+        public int team;
+        public string name;
+        public uint health;
+        public uint lifeState;
+        public float distance;
+        public List<Vector3> bones;
+        public List<Vector2> bones2d;
+        public bool spotted;
+        public short entIndex;
+    }
+
+    public enum BonesIds
+    {
+        Waist = 0,
+        Neck = 5,
+        Head = 6,
+        ShoulderLeft = 8,
+        ForeLeft = 9,
+        HandLeft = 11,
+        ShoulderRight = 13,
+        ForeRight = 14,
+        HandRight = 16,
+        KneeLeft = 23,
+        FeetLeft = 24,
+        KneeRight = 26,
+        FeetRight = 27
+    }
+}
